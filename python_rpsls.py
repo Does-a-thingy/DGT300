cb0ef17068d087087865d0f5e5ae0aa1ccf69518
Does-a-thingy <79290020+Does-a-thingy@users.noreply.github.com>
2023-02-13 12:13:33 +1300

    Update python_rpsls.py

--- python_rpsls.py
+++ python_rpsls.py
@@ -1,15 +1,15 @@
 #importing commands from the 'random' module in a way so I don't have to write 'random.' every time.
 from random import *
 
-# 0 = rock
-# 1 = paper
-# 2 = scissor
-# 3 = lizard
-# 4 = spock
+# 0 = rock > 3, > 2, < 1, < 4
+# 1 = paper < 3, < 2, > 0, > 4
+# 2 = scissor > 3, > 1, < 0, < 4 
+# 3 = lizard > 4, > 1, < 2, < 0
+# 4 = spock < 3, < 1, > 0, > 2
 
 # variable set up
 player_option = 0
-comp_option = 0
+comp_option = randint(0,4)
 
 # first command prompt
 while True:
@@ -22,3 +22,29 @@ while True:
   '''))
   if player_option <= 4 and player_option >= 0:
     break
+  else:
+    print('Please try again')
+
+# results
+if player_option == 0 and (comp_option == 3 or comp_option == 2):
+  print('player wins')
+elif player_option == 0 and (comp_option == 1 or comp_option == 4):
+  print('computer wins')
+elif player_option == 1 and (comp_option == 0 or comp_option == 4):
+  print('player wins')
+elif player_option == 1 and (comp_option == 3 or comp_option == 2):
+  print('computer wins')
+elif player_option == 2 and (comp_option == 1 or comp_option == 3):
+  print('player wins')
+elif player_option == 2 and (comp_option == 0 or comp_option == 4):
+  print('computer wins')
+elif player_option == 3 and (comp_option == 1 or comp_option == 4):
+  print('player wins')
+elif player_option == 3 and (comp_option == 2 or comp_option == 0):
+  print('computer wins')
+elif player_option == 4 and (comp_option == 0 or comp_option == 2):
+  print('player wins')
+elif player_option == 4 and (comp_option == 1 or comp_option == 3):
+  print('computer wins')
+else:
+  print('If your seeing this then it means that my spagetti is Al dente')

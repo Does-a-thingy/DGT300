b2fec47ab643953fe1f041ecf5fa36e7257e895e
Does-a-thingy <79290020+Does-a-thingy@users.noreply.github.com>
2023-02-13 12:50:00 +1300

    Update binary_converter.py

--- binary_converter.py
+++ binary_converter.py
@@ -1,1 +1,29 @@
+# ask for mode
+while True:
+  basetype = int(input('''What would you like to convert from?:
+  '1' for Decimal
+  '2' for Binary
+  '3' for Hexadecimal
+  '4' for Octal
+  '''))
+  if basetype > 5 or < 0:
+    print('please try again')
+  else:
+    
 
+
+
+
+# get decimal number
+def deci_get():
+  while True:
+    deci_number = int(input('Please enter an integer more than -1: '))
+    if deci_number < 0:
+      print('Try again')
+    else:
+      break
+
+# convertion into binary
+def binary_convert(numb):
+  bin_number = bin(numb)
+  print('{} in binary is {}'.format(numb, bin_number))

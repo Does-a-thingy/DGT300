f5693b747621704fc41de9aa0e7cd04b8bb1a3e9
Does-a-thingy <79290020+Does-a-thingy@users.noreply.github.com>
2023-02-13 11:48:32 +1300

    Update python_rpsls.py

--- python_rpsls.py
+++ python_rpsls.py
@@ -1,1 +1,24 @@
+#importing commands from the 'random' module in a way so I don't have to write 'random.' every time.
 from random import *
+
+# 0 = rock
+# 1 = paper
+# 2 = scissor
+# 3 = lizard
+# 4 = spock
+
+# variable set up
+player_option = 0
+comp_option = 0
+
+# first command prompt
+while True:
+  player_option = int(input('''Please type:
+  '0' for rock
+  '1' for paper
+  '2' for scissor
+  '3' for lizard
+  '4' for spock
+  '''))
+  if player_option <= 4 and player_option >= 0:
+    break

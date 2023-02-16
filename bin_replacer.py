7dcc338d5c04e6ec9d3aed6b61d3c213c29a7c94
Does-a-thingy <79290020+Does-a-thingy@users.noreply.github.com>
2023-02-17 12:11:22 +1300

    big update

--- bin_replacer.py
+++ bin_replacer.py
@@ -1,14 +1,15 @@
 values = []
 check_list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+conv = 'to convert'
 
-number = 11
+number = 0
 base = 2
 
-counting = '0'
+counting = ''
 
 def make_values(base):
     global values
-    for i in range(8, -1, -1):
+    for i in range(12, -1, -1):
         values.append(base ** i)
 
 def making_final():
@@ -18,9 +19,20 @@ def making_final():
             counting += "1"
             number -= part
         else:
-            counting += "0"
+            counting += check_list[0]
+            
+def inputing(filler):
+    global loop
+    while True:
+        number = input('What do you want ' + filler + '? ')
+        try:
+            output = int(number)
+            return output
+        except:
+            print('please give me a number')
+        
 
-
+number = inputing('to convert')
 make_values(base)
 making_final()
 print(values)
---
+++ testing_for_higher_numbers.py
@@ -0,0 +1,1 @@
+variable = int(input('give me values'))

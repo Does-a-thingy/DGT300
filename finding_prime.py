9e5e6116eb0a950d10adf73d298cd8dde502b6ce
Does-a-thingy <79290020+Does-a-thingy@users.noreply.github.com>
2023-02-23 10:41:24 +1300

    Update finding_prime.py

--- finding_prime.py
+++ finding_prime.py
@@ -3,31 +3,30 @@
 # 2 count up, and divide by all in list
 # 3 make it have an adjustible end
 
-prime_list = [2, 3]
+prime_list = [2]
+waiting_list = []
+problem = []
 loop = True
 limit = 20
 
-for num in range(limit):
+for i in range(2, limit):
     for item in prime_list:
-        loop = True
-        while loop == True:
-            try:
-                number = num/item
-                output = int(number)
-                prime_list.append(number)
-                if output >= 0:
-                    print('if statement ' + output)
-                    loop = False
-                    break
-                else:
-                    prime_list.append()
-                    print('else statement ' + output)
-                    loop = False
-                    break
-            except:
-                print('except statement ' + str(output))
-                loop = False
-                break
-            break
+        num = i / item
+        if type(num) == float:
+            waiting_list.append(i)
+        elif type(num) == int:
+            if num == 1:
+                waiting_list.append(i)
+                prime_list.append(i)
+            else:
+                waiting_list = []
+        else:
+            print('somethings wrong.')
+            problem.append(i)
+            waiting_list = []
+        
+                
         
 print(prime_list)
+print(problem)
+print(waiting_list)

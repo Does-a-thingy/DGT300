a5821fb1b14df39fdfe1e8d0ba00f6bd379cecdb
Does-a-thingy <79290020+Does-a-thingy@users.noreply.github.com>
2023-03-08 10:03:43 +1300

    Update temperature_converter.py

--- temperature_converter.py
+++ temperature_converter.py
@@ -3,6 +3,17 @@ from tkinter import *
 window = Tk()
 window.title('Temperature converter')
 
-numbers = Frame()
+numbers = Frame(window)
+numbers.grid(row=0, column=0, columnspan=2)
+
+output = Frame(window)
+output.grid(row=0, column=2)
+
+buttons = Frame(window)
+buttons.grid(row=0, column=3)
+
+
+
+
 
 window.mainloop()

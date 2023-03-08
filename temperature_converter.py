eda4513bd744fed647a8ae13fc62fd8c8d568fea
Does-a-thingy <79290020+Does-a-thingy@users.noreply.github.com>
2023-03-09 11:05:00 +1300

    Update temperature_converter.py

--- temperature_converter.py
+++ temperature_converter.py
@@ -4,13 +4,10 @@ window = Tk()
 window.title('Temperature converter')
 
 numbers_frame = Frame(window)
-numbers_frame.grid(row=0, column=0, columnspan=2, sticky='NESW')
 
 output_frame = Frame(window)
-output_frame.grid(row=2, column=0, sticky='NESW')
 
 buttons_frame = Frame(window)
-buttons_frame.grid(row=1, column=0, sticky='NESW')
 
 entered = StringVar()
 entered.set('')
@@ -19,26 +16,54 @@ text_var = StringVar()
 text_var.set('Please input the Temperature')
 
 convt_txt = StringVar()
-convt_txt.set('Converted temperature is: ')
+convt_txt.set('The converted temperature is: ')
 
 def submit():
     try:
         temp_unconv = int(entered.get())
+        entered.set('')
+        return temp_unconv
     except:
         entered.set('')
         text_var.set('Please input the Temperature')
+        
+
+def grid_widget(widget, Rw=0, Clumn=0, clmspn=1, x=10, y=3, stic='NESW'):
+    widget.grid(row=Rw, column=Clumn, columnspan=clmspn, padx=x, pady=y, sticky=stic)
+
+def celc_cmd():
+    tmp_srt = submit()
+    tmp = (tmp_srt * (9/5)) + 32
+    convt_txt.set('The converted temperature is: {}'.format(tmp))
+
+def fahr_cmd():
+    tmp_srt = submit()
+    tmp = (tmp_srt - 32)*(5/9)
+    convt_txt.set('The converted temperature is: {}'.format(tmp))
+
+#have to use command after it's made
+
+grid_widget(numbers_frame, clmspn=2)
+grid_widget(output_frame, Rw=2, clmspn=2)
+grid_widget(buttons_frame, Rw=1, clmspn=2)
 
 
 temp_label = Label(numbers_frame, textvariable=text_var)
-temp_label.grid(row=0, column=0, sticky='NESW')
+grid_widget(temp_label)
 
 temp_entry = Entry(numbers_frame, textvariable=entered)
-temp_entry.grid(row=1, column=0, sticky='NESW')
+grid_widget(temp_entry, Rw=1)
 
 temp_butt = Button(numbers_frame, text='Submit', command=submit)
-temp_butt.grid(row=2, column=0, sticky='NESW')
+grid_widget(temp_butt, Rw=2)
 
 conv_label = Label(output_frame, textvariable=convt_txt)
-conv_label.grid(row=0, column=0, sticky='NESW')
+grid_widget(conv_label)
+
+celc_butt = Button(buttons_frame, text='To celcius', command=celc_cmd)
+grid_widget(celc_butt)
+
+fahr_butt = Button(buttons_frame, text='To Fahrenheit', command=fahr_cmd)
+grid_widget(fahr_butt, Clumn=1)
 
 window.mainloop()

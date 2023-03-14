24193b6582ddf98c33766d2420657ab5cb60947e
Does-a-thingy <79290020+Does-a-thingy@users.noreply.github.com>
2023-03-14 14:55:46 +1300

    Update temperature_converter.py

--- temperature_converter.py
+++ temperature_converter.py
@@ -18,7 +18,7 @@ entered = StringVar()
 entered.set('')
 
 text_var = StringVar()
-text_var.set('Please input the Temperature')
+text_var.set('Please input the Temperature up to 2 decimal places')
 
 convt_txt = StringVar()
 convt_txt.set('The converted temperature is: ')
@@ -44,15 +44,17 @@ help_txt.set('''How to use the program:
   in the new units.''')
 
 histry_lst = []
+histry_str = ''
 
 def submit():
     try:
-        temp_unconv = int(entered.get())
+        place = round(float(entered.get()),2) * 100
+        temp_unconv = int(place)
         entered.set('')
         return temp_unconv
     except:
         entered.set('')
-        text_var.set('Please input the Temperature')
+        text_var.set('Please input the Temperature up to 2 decimal places')
         
 
 def grid_widget(widget, Rw=0, Clumn=0, clmspn=1, x=10, y=3, stic='NESW'):
@@ -61,29 +63,45 @@ def grid_widget(widget, Rw=0, Clumn=0, clmspn=1, x=10, y=3, stic='NESW'):
 def celc_cmd():
     tmp_srt = submit()
     tmp = (tmp_srt * (9/5)) + 32
+    tmp = tmp/100
     convt_txt.set('The converted temperature is: {:.2f} celcius'.format(tmp))
-    history.add(tmp, 'celcius')
+    history.add(tmp_srt/100, 'fahrenheit', tmp, 'celcius')
 
 def fahr_cmd():
     tmp_srt = submit()
     tmp = (tmp_srt - 32)*(5/9)
+    tmp = tmp/100
     convt_txt.set('The converted temperature is: {:.2f} fahrenheit'.format(tmp))
-    history.add(tmp, 'fahrenheit')
+    history.add(tmp_srt/100, 'celcius', tmp, 'fahrenheit')
 
 class history:
-    global histry_lst
-    def add(num, typ):
-        info = [num, typ]
-        histry_lst.append(' '.join(info))
-        print(histry_lst)
+    def add(bas, tpe, num, typ):
+        global histry_lst
+        histry_lst.append('{} {} was converted to {} {}'.format(round(bas, 2), tpe, round(num, 2), typ))
+        
+        #info = [str(round(num, 2)), typ]
+        #histry_lst.append(' '.join(info))
+        #print(histry_lst)
     
     def open():
+        global histry_lst, histry_str 
         histry = Toplevel(window)
         histry.title('History')        
-        histry_str = '\n'.join(histry_lst)
+        histry_str = ' \n '.join(histry_lst)
         histry_txt = StringVar()
         histry_txt.set(histry_str)
         histry_lab = Label(histry, textvariable=histry_txt)
+        grid_widget(histry_lab, 1)
+        htop_lab = Label(histry, text='Conversion history:')
+        grid_widget(htop_lab)
+        save_butt = Button(histry, text='Save history', command=history.save_t_file)
+        grid_widget(save_butt, 2)
+        
+    def save_t_file():
+        global histry_str
+        with open('temp_history.txt', 'a') as f:
+            f.write(histry_str)
+            f.write('\n')
 
 class helping:
     def open():

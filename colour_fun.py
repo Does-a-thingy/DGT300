0cdba5e5ab74e15e06081ce0722ba4c213516af7
Does-a-thingy <79290020+Does-a-thingy@users.noreply.github.com>
2023-03-30 10:46:38 +1300

    Update colour_fun.py

--- colour_fun.py
+++ colour_fun.py
@@ -4,10 +4,11 @@ from bin_rep_funct import *
 
 w = Tk()
 
-colour = []
+colour = '#FFFFFF'
+color = '#FFFFFF'
 
-top_f = Frame(w, bg=colour[a])
+top_f = Frame(w, bg=colour)
 grd_wid(top_f)
-bot_f = Frame(w, bg=colour[b])
+bot_f = Frame(w, bg=color)
 grd_wid(bot_f)
 

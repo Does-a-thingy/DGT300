f5ae684a65ece9acda1bde10eef0dadc6e7c7db9
Does-a-thingy <79290020+Does-a-thingy@users.noreply.github.com>
2023-03-30 11:06:19 +1300

    mayhem

--- bin_rep_funct.py
+++ bin_rep_funct.py
@@ -1,22 +1,12 @@
-values = []
+values = [1, 16]
 check_list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 conv = 'to convert'
 
 number = 0
-base = 2
-end_value = 1
+base = 16
 counting = ''
 intre = True
 
-def max_range(base, numb):
-    powered = 1
-    i = 0
-    limit = 1
-    while limit > 0:
-        i += 1
-        powered = base ** i
-        limit = numb//powered
-    return i
 
 def make_values(base, limit):
     global values
@@ -33,30 +23,10 @@ def making_final():
         else:
             counting += check_list[0]
 
-def inputing(filler, check):
-    while True:
-        number = input('What do you want ' + filler)
-        try:
-            output = int(number)
-            if output >= 0:
-                if check == 0:
-                    return output
-                elif check == 1 and output >= 2 and output <= 36:
-                    return output
-                else:
-                    print('please give me a valid number')
-            else:
-                print('please give me a valid number')
-        except:
-            print('please give me a valid number')
-
 def main():
-    intre = True
+    
     while intre is True:
-        number = inputing('to convert? make sure it is more than -1: ', 0)
-        base = inputing('for base? make sure it is between 1 and 37: ', 1)
-        end_value = max_range(base, number)
-        make_values(base, end_value)
+        make_values(base, 1)
         making_final()
         print(values)
         print(counting)
--- colour_fun.py
+++ colour_fun.py
@@ -5,10 +5,38 @@ from bin_rep_funct import *
 w = Tk()
 
 colour = '#FFFFFF'
-color = '#FFFFFF'
+color = '#000000'
+
+number1 = 0
+number2 = 0
+number3 = 0
+
+number4 = 255
+number5 = 255
+number6 = 255
 
 top_f = Frame(w, bg=colour)
 grd_wid(top_f)
 bot_f = Frame(w, bg=color)
 grd_wid(bot_f)
 
+def cnt_loop(numb, limit, reval, step):
+    if numb != limit:
+        numb += step
+        return numb
+    elif numb == limit:
+        numb = reval
+        return numb
+
+w.mainloop()
+
+while True:
+    number1 = cnt_loop(number1, 255, 0, 3)
+    number2 = cnt_loop(number2, 0, 255, -5)
+    number3 = cnt_loop(number3, 255, 0, 1)
+    
+    number4 = cnt_loop(number4, 0, 255, -1)
+    number5 = cnt_loop(number5, 255, 0, 5)
+    number6 = cnt_loop(number6, 0, 255, -3)
+    
+    

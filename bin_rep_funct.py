9dd68c34f3cefbc7047db433bc7a5d3ebfa179f1
Does-a-thingy <79290020+Does-a-thingy@users.noreply.github.com>
2023-03-31 12:05:49 +1300

    got it changing

--- bin_rep_funct.py
+++ bin_rep_funct.py
@@ -1,31 +1,15 @@
 values = [16, 1]
 check_list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
 
-number = 0
-base = 16
-counting = ''
-intre = True
-
-def making_final():
-    global counting, values, number, check_list
+def making_final(number):
+    global values, check_list
+    counting = ''
     for part in values:
         if number >= part:
-            num = number//part #gives whole number after division round down, ignore left over
-            counting += check_list[num] # allows for up to base 36
-            number -= part*num
+            num = number//part #gives whole number after division round down, ignores leftovers
+            counting += check_list[num] # pulls from letters and numbers
+            number -= part*num # gets rid of parts times num
         else:
-            counting += check_list[0]
+            counting += check_list[0] # will slap a zero on it if theres nothing
+    return counting
 
-def main():
-    
-    while intre is True:
-        making_final()
-        if inp == 'yes':
-            number = 0
-            base = 2
-            end_value = 1
-            intre = True
-            counting = ''
-            values = []
-        else:
-            intre = False
--- colour_fun.py
+++ colour_fun.py
@@ -18,7 +18,12 @@ number6 = 255
 top_f = Frame(w, bg=colour)
 grd_wid(top_f)
 bot_f = Frame(w, bg=color)
-grd_wid(bot_f)
+grd_wid(bot_f, 1)
+
+top_l = Label(top_f, text='')
+grd_wid(top_l)
+bot_l = Label(bot_f, text='')
+grd_wid(bot_l)
 
 def cnt_loop(numb, limit, reval, step):
     if numb != limit:
@@ -27,16 +32,28 @@ def cnt_loop(numb, limit, reval, step):
     elif numb == limit:
         numb = reval
         return numb
-
-w.mainloop()
-
-while True:
+def colour_changing():
+    global number1, number2, number3, number4, number5, number6
     number1 = cnt_loop(number1, 255, 0, 3)
     number2 = cnt_loop(number2, 0, 255, -5)
     number3 = cnt_loop(number3, 255, 0, 1)
+    hex1 = making_final(number1)
+    hex2 = making_final(number2)
+    hex3 = making_final(number3)
+    colour = str('#' + hex1 + hex2 + hex3)
+    top_f.config(background=colour)
     
     number4 = cnt_loop(number4, 0, 255, -1)
     number5 = cnt_loop(number5, 255, 0, 5)
     number6 = cnt_loop(number6, 0, 255, -3)
+    hex4 = making_final(number4)
+    hex5 = making_final(number5)
+    hex6 = making_final(number6)
+    color = str('#' + hex4 + hex5 + hex6)
+    bot_f.config(background=color)
     
-    
+    w.after(100, colour_changing) # runs the command again after 100 mili-seconds
+
+colour_changing()
+
+w.mainloop()

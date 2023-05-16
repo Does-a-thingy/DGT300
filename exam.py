26f4be9c2dd398a955e68a5ba0576aeb9a603b03
Does-a-thingy <79290020+Does-a-thingy@users.noreply.github.com>
2023-05-17 10:05:04 +1200

    Update exam.py

--- exam.py
+++ exam.py
@@ -1,7 +1,8 @@
 from collation_oliver import *
 
 win = Tk()
-win['background']='#EFE7BC'
+win.configure(bg='#EFE7BC')
+win.title('Movie Theatre')
 
 titlfr = Frame(win, bg='#EFE7BC')
 grd_wid(titlfr, iy=0, ix=0, y=0, x=0)
@@ -11,27 +12,55 @@ grd_wid(frm1, 1, iy=0, ix=0, y=0, x=0)
 
 total = 0
 
+time1 = StringVar()
+time2 = StringVar()
+time3 = StringVar()
+
 def price(tiknum, pric):
     global total
     total += tiknum * pric
 
-# use blanks to center things
+def mov1cmd():
+    time1.set('10:45 AM')
+    time2.set('2:20 PM')
+    time3.set('4:60 PM')
+    grd_wid(time1b, x=10, y=10)
+    grd_wid(time2b, 0,1,x=10,y=10)
+    grd_wid(time3b, 0,2,x=10,y=10)
+
+def mov2cmd():
+    time1.set('10:45 AM')
+    time2.set('2:20 PM')
+    time3.set('4:60 PM')
+    grd_wid(time1b, x=10, y=10)
+    grd_wid(time2b, 0,1,x=10,y=10)
+    grd_wid(time3b, 0,2,x=10,y=10)
+
+def mov3cmd():
+    time1.set('10:45 AM')
+    time2.set('2:20 PM')
+    time3.set('4:60 PM')
+    grd_wid(time1b, x=10, y=10)
+    grd_wid(time2b, 0,1,x=10,y=10)
+    grd_wid(time3b, 0,2,x=10,y=10)    
+
+
 blank = StringVar()
 blank.set('')
 
-title = Label(titlfr, text='THIS IS THE TITLE', bg='#74BDCB', font=('lucid', 20))
-grd_wid(title, 0, 1, clmspn=3, y=10, x=10)
+titlem = Label(titlfr, text='Movie Theatre', bg='#74BDCB', font=('lucid', 28))
+grd_wid(titlem, 0, 1, clmspn=3, y=10, x=10)
 titl = Label(titlfr, textvariable=blank, bg='#EFE7BC')
 grd_wid(titl, x=10, ix=10)
 titr = Label(titlfr, textvariable=blank, bg='#EFE7BC')
 grd_wid(titr, 0, 4, x=10, ix=10)
 
-movi1 = Label(frm1, text='MOVIE 1', bg='#FFA384')
-grd_wid(movi1, Clumn=1, x=10, ix=10, y=20)
-movi2 = Label(frm1, text='MOVIE 2', bg='#FFA384')
-grd_wid(movi2, Clumn=2, x=10, ix=10, y=20)
-movi3 = Label(frm1, text='MOVIE 3', bg='#FFA384')
-grd_wid(movi3, Clumn=3, x=10, ix=10, y=20)
+movi1 = Button(frm1, text='MOVIE 1', bg='#FFA384', command=mov1cmd)
+grd_wid(movi1, Clumn=1, x=10, ix=10, y=20, iy=5)
+movi2 = Button(frm1, text='MOVIE 2', bg='#FFA384', command=mov2cmd)
+grd_wid(movi2, Clumn=2, x=10, ix=10, y=20, iy=5)
+movi3 = Button(frm1, text='MOVIE 3', bg='#FFA384', command=mov3cmd)
+grd_wid(movi3, Clumn=3, x=10, ix=10, y=20, iy=5)
 
 movl = Label(frm1, textvariable=blank, bg='#EFE7BC')
 grd_wid(movl, x=10, ix=10)
@@ -39,9 +68,16 @@ movr = Label(frm1, textvariable=blank, bg='#EFE7BC')
 grd_wid(movr, 0, 4, x=10, ix=10)
 
 timefrm = Frame(frm1, bg='#EFBCDE')
-grd_wid(timefrm, 1, y=10, x=10, clmspn=5)
+grd_wid(timefrm, 1, 1, y=10, x=10, clmspn=3)
 
-testlab = Label(timefrm, text="TEST", bg='#74BDCB')
-grd_wid(testlab, x=10, y=10)
+time1b = Label(timefrm, textvariable=time1, bg='#74BDCB')
+grd_wid(time1b, x=10, y=10)
+time2b = Label(timefrm, textvariable=time2, bg='#74BDCB')
+grd_wid(time2b, 0,1,x=10,y=10)
+time3b = Label(timefrm, textvariable=time3, bg='#74BDCB')
+grd_wid(time3b, 0,2,x=10,y=10)
+
+botspc= Label(win, textvariable=blank, bg='#EFE7BC')
+grd_wid(botspc, 2)
 
 win.mainloop()

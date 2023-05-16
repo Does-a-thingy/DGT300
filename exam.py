74c25742db108f9109fb87a3c3a60fe24b46ff3e
Does-a-thingy <79290020+Does-a-thingy@users.noreply.github.com>
2023-05-16 14:45:40 +1200

    Update exam.py

--- exam.py
+++ exam.py
@@ -15,9 +15,15 @@ def price(tiknum, pric):
     total += tiknum * pric
 
 # use blanks to center things
+blank = StringVar()
+blank.set('')
 
 title = Label(titlfr, text='THIS IS THE TITLE', bg='#c8a951', font=('lucid', 20))
-grd_wid(title, y=10, x=10)
+grd_wid(title, 0, 1, clmspn=3, y=10, x=10)
+titl = Label(titlfr, textvariable=blank, bg='#663046')
+grd_wid(titl, x=10, ix=10)
+titr = Label(titlfr, textvariable=blank, bg='#663046')
+grd_wid(titr, 0, 4, x=10, ix=10)
 
 movi1 = Label(frm1, text='MOVIE 1', bg='#FFCE9F')
 grd_wid(movi1, Clumn=1, x=10, ix=10)
@@ -26,13 +32,13 @@ grd_wid(movi2, Clumn=2, x=10, ix=10)
 movi3 = Label(frm1, text='MOVIE 3', bg='#FFCE9F')
 grd_wid(movi3, Clumn=3, x=10, ix=10)
 
-movl = Label(frm1, text='', bg='#be93d4')
+movl = Label(frm1, textvariable=blank, bg='#be93d4')
 grd_wid(movl, x=10, ix=10)
-movr = Label(frm1, text='', bg='#be93d4')
-grd_wid(movr, 0, 5, x=10, ix=10)
+movr = Label(frm1, textvariable=blank, bg='#be93d4')
+grd_wid(movr, 0, 4, x=10, ix=10)
 
 timefrm = Frame(frm1, bg='#E6E6E6')
-grd_wid(timefrm, 1, y=20, x=10, ix=5, iy=10, clmspn=3)
+grd_wid(timefrm, 1, y=20, x=10, ix=5, iy=10, clmspn=5)
 
 testlab = Label(timefrm, text="TEST")
 grd_wid(testlab, x=20, y=10)

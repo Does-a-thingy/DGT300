33504e50b6ad3f0f64f25cb87a8fced530cf09ef
Does-a-thingy <79290020+Does-a-thingy@users.noreply.github.com>
2023-07-28 12:25:26 +1200

    turtle breeding

--- something_crazy.py
+++ something_crazy.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from turtle import *
 import random
+import time
 
 # https://stackoverflow.com/questions/54246872/how-to-combine-tkinter-and-turtle
 # Use rawturtle instead of turtle
@@ -19,12 +20,13 @@ first.speed(random.randint(1, 3))
 first.penup()
 first.fillcolor('green')
 first.shape("turtle")
+first.goto(random.randint(-50, 50), random.randint(-50, 50))
 second = first.clone()
-second.ht()
+second.goto(random.randint(-50, 50), random.randint(-50, 50))
+third = second.clone()
+third.goto(random.randint(-50, 50), random.randint(-50, 50))
 x = False
 b = 0
-fangle = first.towards(0, 0)
-sangle = second.towards(0, 0)
 
 box.lt(90)
 box.penup()
@@ -102,14 +104,6 @@ screen.configure(bg='green')
 check_frame = Frame(window)
 check_frame.grid(padx=2, pady=2, row=11, column=0, rowspan=1, columnspan=5)
 
-turt_num = IntVar()
-turt_num.set(2)
-
-one_turt_ck = Checkbutton(check_frame, variable=turt_num, onvalue=1, offvalue=2, text='One turtle')
-one_turt_ck.grid(row=0)
-two_turt_ck = Checkbutton(check_frame, variable=turt_num, onvalue=2, offvalue=1, text='Two turtles')
-two_turt_ck.grid(row=0, column=2)
-
 multi = IntVar()
 multi.set(1)
 
@@ -118,13 +112,25 @@ multi_turt.grid(row=0, column=3, padx=10)
 #start of action
 first.lt(random.randint(0, 359))
 second.lt(random.randint(0, 359))
+third.lt(random.randint(0, 359))
 
-turtle_list = [first, second]
+turtle_list = [first, second, third]
 
-def make_turtle():
-    global first
-    new_turtle = first.clone()
+def make_turtle(animal):
+    new_turtle = animal.clone()
     return new_turtle
+
+def turtle_multiply(animal):
+    global turtle_list
+    turtle_population = len(turtle_list) + 1
+    self_index = turtle_list.index(animal)    
+    for i in range(0, turtle_population):
+        if i-1 != self_index and i-1 != 0:
+            print(i-1)
+            print(self_index)
+            if animal.ycor() == turtle_list[i-1].ycor() and animal.xcor() == turtle_list[i-1].xcor():
+                turtle_list.append(make_turtle(animal))
+                print('Baby')
 
 def turtle_positioning(name, fd_low, fd_high, lt_low, lt_high, spd_low, spd_high):
     if name.xcor() >= 190:
@@ -154,17 +160,10 @@ def turtle_positioning(name, fd_low, fd_high, lt_low, lt_high, spd_low, spd_high
 while x is False:
     # this controls the turtles movements
     # .get() is needed for calling variables for if statements code, crashes otherwise
-    if turt_num.get() == 1:
-        turtle_list[1].ht()
-        turtle_positioning(turtle_list[0], 0, 6, -40, 40, 1, 2)
-    elif turt_num.get() == 2 and multi.get() == 0:
-        turtle_list[1].st()
+    if multi.get() == 0:
         turtle_positioning(turtle_list[0], 0, 6, -40, 40, 1, 2)
         turtle_positioning(turtle_list[1], 0, 8, -30, 30, 1, 4)
-    elif turt_num.get() == 2 and multi.get() == 1:
+    elif multi.get() == 1:
         for animal in turtle_list:
-            turtle_list[1].st()
-            turtle_positioning(animal, 0, 7, -35, 35, 1, 3)
-            if first.ycor() == second.ycor() or first.xcor() == second.xcor():
-                turtle_list.append(make_turtle())
-                print('Baby')
+            turtle_positioning(animal, 0, 7, -35, 35, 0, 3)
+            turtle_multiply(animal)

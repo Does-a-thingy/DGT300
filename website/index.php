6486c12f8158992d15178e56ce56cf97c5c42f43
Does-a-thingy <79290020+Does-a-thingy@users.noreply.github.com>
2023-08-02 10:05:11 +1200

    major changes

---
+++ website/index.php
@@ -0,0 +1,6 @@
+<!doctype html>
+<html>
+<php> echo file_get_contents("C:\Users\oliver.macclure\OneDrive - Henderson High School\Documents\GitHub\DGT300\website/header.html") </php>
+<php> echo file_get_contents("C:\Users\oliver.macclure\OneDrive - Henderson High School\Documents\GitHub\DGT300\website/body.html") </php>
+<php> echo file_get_contents("C:\Users\oliver.macclure\OneDrive - Henderson High School\Documents\GitHub\DGT300\website/footer.html") </php>
+</html>

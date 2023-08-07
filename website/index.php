f111dbd593bc38f98fecb080e4ba2f930936bf09
Does-a-thingy <79290020+Does-a-thingy@users.noreply.github.com>
2023-08-07 13:25:07 +1200

    WCDFLCD

--- website/index.php
+++ website/index.php
@@ -6,12 +6,14 @@
         <link rel="stylesheet" href="index.css">
         <title>Lost and found</title>
     </head>
-    <body>
-        <p></p>
-        <?php
-            echo file_get_contents("header.html");
-            echo file_get_contents("body.html");
-            echo file_get_contents("footer.html");
-        ?>
-    </body>
+    <?php
+    $hostName = "localhost";
+    $userName = "root";
+    $password = "";
+    
+        echo file_get_contents("header.html");
+        echo file_get_contents("body.html");
+        echo file_get_contents("footer.html");
+    ?>
+
 </html>

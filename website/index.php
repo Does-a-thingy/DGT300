239b34534f1fdbb33ee7dc7f54486f8876611077
Does-a-thingy <79290020+Does-a-thingy@users.noreply.github.com>
2023-08-10 10:43:01 +1200

    yesy

--- website/index.php
+++ website/index.php
@@ -7,13 +7,40 @@
         <title>Lost and found</title>
     </head>
     <?php
-    $hostName = "localhost";
-    $userName = "root";
-    $password = "";
-    
+        $hostName = "localhost";
+        $userName = "root";
+        $password = "";
+        $database = "crud";
+
+        $conn = mysqli_connect($hostName, $userName, $password, $database)
+            //Host name, User name, password, database names
+
+        //$db = $conn
+        //$table = ""
+
+        if(isset($_POST['insert_btn'])){
+            $name = $_POST['user_name']
+            $place = $_POST['place']
+            $date = $_POST['date']
+            $object = $_POST['object']
+            $discript = $_POST['discription']
+
+            $insert = "INSERT INTO 
+            information(date,object,place)
+            VALUES('$name', '$place', '$date', '$object', '$discript')";
+
+            $run_insert = mysqli_query($conn, $insert);
+
+            if($run_insert) {
+                echo "Record inserted";
+            }
+            else {
+                echo "Try again";
+            }
         echo file_get_contents("header.html");
         echo file_get_contents("body.html");
         echo file_get_contents("footer.html");
+        }
     ?>
 
 </html>
